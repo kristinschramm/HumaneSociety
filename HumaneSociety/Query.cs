cbe59cdfd06a92924877695459411d235d95ab15
kristinschramm <kristin.schramm@gmail.com>
2018-05-25 10:43:20 -0500

    resolve conflict

--- HumaneSociety/Query.cs
+++ HumaneSociety/Query.cs
@@ -8,13 +8,13 @@ namespace HumaneSociety
 {
     public static class Query
     {
-        
+
         //member variables
 
         //constructor
 
         //member method
-   
+
         static public void RunEmployeeQueries(Employee employee, string crud)
         {
 
@@ -81,6 +81,75 @@ namespace HumaneSociety
         }
 
         internal static void UpdateLastName(Client client)
+        {
+            throw new NotImplementedException();
+        }
+        internal static object GetPendingAdoptions()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal static void UpdateAdoption(bool v, ClientAnimalJunction clientAnimalJunction)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal static object GetShots(Animal animal)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal static void UpdateShot(string v, Animal animal)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal static void EnterUpdate(Animal animal, Dictionary<int, string> updates)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal static void RemoveAnimal(Animal animal)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal static int? GetBreed()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal static int? GetDiet()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal static int? GetLocation()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal static void AddAnimal(Animal animal)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal static Employee EmployeeLogin(string userName, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal static Employee RetrieveEmployeeUser(string email, int employeeNumber)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal static void AddUsernameAndPassword(Employee employee)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal static bool CheckEmployeeUserNameExist(string username)
         {
             throw new NotImplementedException();
         }
