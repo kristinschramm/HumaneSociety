8e55356fa9a667fcfe44ff2a35a06d14374ee8bb
ckloskey <you@example.com>
2018-05-25 10:28:37 -0500

    fixing merge conflicts

--- HumaneSociety/Properties/Settings.Designer.cs
+++ HumaneSociety/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace HumaneSociety.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "14.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.6.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -31,6 +31,17 @@ namespace HumaneSociety.Properties {
         public string HumaneSocietyConnectionString {
             get {
                 return ((string)(this["HumaneSocietyConnectionString"]));
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=DESKTOP-5CVCB9U;Initial Catalog=HumaneSociety;Integrated Security=Tru" +
+            "e;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False")]
+        public string HumaneSocietyConnectionString1 {
+            get {
+                return ((string)(this["HumaneSocietyConnectionString1"]));
             }
         }
     }
--- HumaneSociety/Query.cs
+++ HumaneSociety/Query.cs
@@ -8,8 +8,74 @@ namespace HumaneSociety
 {
     public static class Query
     {
-        
-        
-      
+        internal static object GetPendingAdoptions()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal static void UpdateAdoption(bool v, ClientAnimalJunction clientAnimalJunction)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal static object GetShots(Animal animal)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal static void UpdateShot(string v, Animal animal)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal static void EnterUpdate(Animal animal, Dictionary<int, string> updates)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal static void RemoveAnimal(Animal animal)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal static int? GetBreed()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal static int? GetDiet()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal static int? GetLocation()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal static void AddAnimal(Animal animal)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal static Employee EmployeeLogin(string userName, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal static Employee RetrieveEmployeeUser(string email, int employeeNumber)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal static void AddUsernameAndPassword(Employee employee)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal static bool CheckEmployeeUserNameExist(string username)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
